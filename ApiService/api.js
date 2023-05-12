2954dcb242fcd56dc0ecd089a3c3395f4616b6dd
Samuel Cardoso <samuelcm456@gmail.com>
2023-05-11 21:53:25 -0300

    a

--- ApiService/api.js
+++ ApiService/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://servidorvercel-beta.vercel.app'
+    baseURL: 'https://servidorvercel-beta.vercel.app/'
     //baseURL: 'http://192.168.1.83:3000'
     //baseURL: 'http://192.168.1.110:3000'
 });
